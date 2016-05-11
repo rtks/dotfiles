9067c6523053e152fcefa06d35fd09ff8d780730
rtks <rtks@users.noreply.github.com>
2016-05-12 03:12:32 +0900

    fishのconf.dの読み込みは2.3.0未満

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,6 @@
-for file in ~/.config/fish/conf.d/*.fish
-    source $file
+switch "$FISH_VERSION"
+    case 2.2.0 2.1.2 2.1.1 2.1.0 2.0.0
+        for file in ~/.config/fish/conf.d/*.fish
+            source $file
+        end
 end
