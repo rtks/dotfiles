17acc46a9274190ca3bb1f38aa3600dbe5e6229b
rtks <rtks@users.noreply.github.com>
2016-05-11 09:06:45 +0100

    fish shellを導入してfishermanでプラグインを管理

---
+++ config/fish/functions/git-fixup.fish
@@ -0,0 +1,24 @@
+function git-fixup
+    set MAX_LOG_COUNT 30
+    
+    if git diff --cached --quiet
+        set commits "No staged changes. Use git add -p to add them."
+        set ret 1
+    else
+        set commits (git log --oneline -n "$MAX_LOG_COUNT")
+        set ret $status
+    end
+    
+    if [ "$ret" != 0 ]
+        set headline (echo "$commits" | head -n1)
+        if [ "$headline" = "No staged changes. Use git add -p to add them." ]
+            echo "$headline" >&2
+        end
+        return "$ret"
+    end
+    
+    for i in $commits; echo $i; end | fzf-tmux | read -l line
+    [ "$status" = 0 -a "$line" != "" ]; or return "$status"
+    
+    git commit --fixup (echo "$line" | awk '{print $1}')
+end
