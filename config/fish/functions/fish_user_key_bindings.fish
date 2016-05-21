821bbb85576e2c79b2f9f9ff172b388bc09b0f9f
rtks <rtks@users.noreply.github.com>
2016-05-22 05:35:27 +0900

    fishキーバインドを整理

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -3,14 +3,22 @@ function fish_user_key_bindings
   if type fzf >/dev/null ^&1
     bind \ct '__fzf_ctrl_t'
     bind \cr '__fzf_ctrl_r'
-    bind \cq '__fzf_z'
     bind \ec '__fzf_alt_c'
     if bind -M insert > /dev/null ^ /dev/null
       bind -M insert \ct '__fzf_ctrl_t'
       bind -M insert \cr '__fzf_ctrl_r'
-      bind -M insert \cq '__fzf_z'
       bind -M insert \ec '__fzf_alt_c'
     end
   end
+  ### fzf ###
+  if type fzf >/dev/null ^&1
+    bind \cq '__fzf_z'
+    if bind -M insert >/dev/null ^/dev/null
+      bind -M insert \cq '__fzf_z'
+    end
+  end
   bind \cx '__global_alias'
+  if bind -M insert >/dev/null ^/dev/null
+    bind -M insert \cx '__global_alias'
+  end
 end
