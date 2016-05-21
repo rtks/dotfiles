ef0a082dcd8aa1e94580c29a5d3cafdb7120a3bb
rtks <rtks@users.noreply.github.com>
2016-05-22 02:54:41 +0900

    fishのgit-fixupの補完の関数を整理

--- config/fish/completions/git-fixup.fish
+++ config/fish/completions/git-fixup.fish
@@ -1,8 +1,6 @@
 function __git_fixup_commits
-  set -l cnt
-  for msg in (command git log --pretty=tformat:"%h\t%s" -n20)
-    printf "%02d-$msg\n" (count $cnt)
-    set cnt $cnt 1
+  for i in (command git log --pretty=tformat:"%h\t%s" -n20 | awk '{printf("%02d-%s\n", NR-1, $0)}')
+    printf "$i\n"
   end
 end
 complete -f -c git-fixup -a '(__git_fixup_commits)'
