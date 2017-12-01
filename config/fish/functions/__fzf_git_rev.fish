2a1bda1702a15357569716377bf2a1439ec4ea11
rtks <rtks@users.noreply.github.com>
2017-12-01 20:40:02 +0900

    fzfでgit revを選択する

---
+++ config/fish/functions/__fzf_git_rev.fish
@@ -0,0 +1,5 @@
+function __fzf_git_rev
+	git log --graph --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" | __fzfcmd $FZF_DEFAULT_OPT | sed 's#^[^0-9a-f]*##' | awk '{print $1}' | read -l sel
+test "$sel"; and commandline -it -- $sel
+commandline -f repaint
+end
--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,22 +1,10 @@
 function fish_user_key_bindings
-  ### fzf ###
-  if type fzf >/dev/null ^&1
-    bind \ct '__fzf_ctrl_t'
-    bind \cr '__fzf_ctrl_r'
-    bind \ec '__fzf_alt_c'
-    if bind -M insert > /dev/null ^ /dev/null
-      bind -M insert \ct '__fzf_ctrl_t'
-      bind -M insert \cr '__fzf_ctrl_r'
-      bind -M insert \ec '__fzf_alt_c'
-    end
-  end
-  ### fzf ###
   if type fzf >/dev/null ^&1
     bind \cq '__fzf_z'
-    bind \eg '__fzf_alt_g'
+    bind \eg '__fzf_git_rev'
     if bind -M insert >/dev/null ^/dev/null
       bind -M insert \cq '__fzf_z'
-      bind -M insert \eg '__fzf_alt_g'
+      bind -M insert \eg '__fzf_git_rev'
     end
   end
   bind \cx '__global_alias'
