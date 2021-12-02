e70a1f0addf2d8f975dd4e7e4b60cdf0845fbefd
rtks <rtks@users.noreply.github.com>
2021-12-02 18:29:27 +0900

    set_abbr_customを追加

---
+++ .config/fish/functions/set_abbr_custom.fish
@@ -0,0 +1,6 @@
+# Defined in /tmp/fish.R9HA8B/set_abbr_custom.fish @ line 1
+function set_abbr_custom
+    abbr gfi 'git fixup'
+    abbr gfia 'git add --all; git-fixup'
+    abbr gau 'git add --update'
+end
