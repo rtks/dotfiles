fc01d5d6d9905f71e0a0157f7dd6a0e9d9668abd
rtks <rtks@users.noreply.github.com>
2022-08-28 00:53:11 +0900

    docker composeのabbr

--- .config/fish/functions/set_abbr_custom.fish
+++ .config/fish/functions/set_abbr_custom.fish
@@ -4,9 +4,9 @@ function set_abbr_custom
     abbr gfia 'git add --all; git-fixup'
     abbr gau 'git add --update'
 
-    abbr dco 'docker-compose'
-    abbr dcup 'docker-compose up -d'
-    abbr dcb 'docker-compose up -d --build'
-    abbr dce 'docker-compose exec'
-    abbr dclo 'docker-compose logs -ft --tail=1000'
+    abbr dco 'docker compose'
+    abbr dcup 'docker compose up -d'
+    abbr dcb 'docker compose up -d --build'
+    abbr dce 'docker compose exec'
+    abbr dclo 'docker compose logs -ft --tail=1000'
 end
