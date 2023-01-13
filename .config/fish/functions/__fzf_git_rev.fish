4234f11a726b0db88ddc3980d835763071b9d7fa
rtks <rtks@users.noreply.github.com>
2023-01-13 18:18:24 +0900

    fzfでgit rev検索を修正

--- .config/fish/functions/__fzf_git_rev.fish
+++ .config/fish/functions/__fzf_git_rev.fish
@@ -1,5 +1,19 @@
 function __fzf_git_rev
-	git log --graph --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" --all | eval (__fzfcmd) $FZF_DEFAULT_OPT | sed 's#^[^0-9a-f]*##' | awk '{print $1}' | read -l sel
-test "$sel"; and commandline -it -- $sel
-commandline -f repaint
+    set MAX_LOG_COUNT 100
+    
+    set commits (git log --oneline --decorate --color --graph -n "$MAX_LOG_COUNT" --all)
+    set ret $status
+    
+    if [ "$ret" != 0 ]
+        set headline (echo "$commits" | head -n1)
+        if [ "$headline" = "No staged changes. Use git add -p to add them." ]
+            echo "$headline" >&2
+        end
+        return "$ret"
+    end
+    
+    for i in $commits; echo $i; end | fzf-tmux --preview 'git show --color $(echo {} | grep -o "[0-9a-f]\{7,40\}")' | grep -o "[0-9a-f]\{7,40\}"| read -l line
+    [ "$status" = 0 -a "$line" != "" ]; or return "$status"
+    commandline -i "$line"
+    commandline -f repaint
 end
