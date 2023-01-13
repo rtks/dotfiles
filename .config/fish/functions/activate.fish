5a6e8bb79e8e6dff859b05e1ab85db5a4e187b0f
rtks <rtks@users.noreply.github.com>
2023-01-13 18:23:25 +0900

    activateでサブシェルを起動に変更

--- .config/fish/functions/activate.fish
+++ .config/fish/functions/activate.fish
@@ -1,7 +1,7 @@
 function activate
 	if test -f "$argv[1]/bin/activate.fish"
-		builtin source "$argv[1]/bin/activate.fish"
+		command fish --login --init-command="source '$argv[1]/bin/activate.fish'"
 	else if test -f "$HOME/.envs/$argv[1]/bin/activate.fish"
-		builtin source "$HOME/.envs/$argv[1]/bin/activate.fish"
+		command fish --login --init-command="source '$HOME/.envs/$argv[1]/bin/activate.fish'"
 	end
 end
