7f9f3c3139e62fc0766f4f6f2b09aebd59cb8e52
rtks <rtks@users.noreply.github.com>
2023-01-13 18:22:08 +0900

    fish 3.6対応 global abbrはデフォルト機能を使う

---
+++ .config/fish/conf.d/set_abbr_custom.fish
@@ -0,0 +1,18 @@
+abbr -a gfi 'git fixup'
+abbr -a gfia 'git add --all; git-fixup'
+abbr -a gau 'git add --update'
+
+abbr -a t 'tig --all'
+
+abbr -a dco 'docker-compose'
+abbr -a dcup 'docker-compose up -d'
+abbr -a dcb 'docker-compose up -d --build'
+abbr -a dce 'docker-compose exec'
+abbr -a dclo 'docker-compose logs -ft --tail=1000'
+
+abbr -a L --position anywhere "| less"
+abbr -a H --position anywhere "| head"
+abbr -a T --position anywhere "| tail"
+abbr -a G --position anywhere "| grep"
+abbr -a F --position anywhere "| fzf-tmux"
+abbr -a N --position anywhere "| nkf"
