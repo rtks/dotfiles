e5932ba85fd741768702e77fb5580fda6c4102f6
rtks <rtks@users.noreply.github.com>
2023-04-05 11:17:01 +0900

    docker composeのabbrをpluginのものに変更

--- .config/fish/conf.d/set_abbr_custom.fish
+++ .config/fish/conf.d/set_abbr_custom.fish
@@ -4,11 +4,11 @@ abbr -a gau 'git add --update'
 
 abbr -a t 'tig --all'
 
-abbr -a dco 'docker-compose'
-abbr -a dcup 'docker-compose up -d'
-abbr -a dcb 'docker-compose up -d --build'
-abbr -a dce 'docker-compose exec'
-abbr -a dclo 'docker-compose logs -ft --tail=1000'
+abbr -a dco 'docker compose'
+abbr -a dcup 'docker compose up -d'
+abbr -a dcb 'docker compose up -d --build'
+abbr -a dce 'docker compose exec'
+abbr -a dclo 'docker compose logs -ft --tail=1000'
 
 abbr -a L --position anywhere "| less"
 abbr -a H --position anywhere "| head"
