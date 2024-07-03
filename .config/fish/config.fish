0d25746533396bfeadc7ef0ce1756646e17ea3cf
rtks <rtks@users.noreply.github.com>
2024-07-03 12:18:55 +0900

    fisher_pathを設定

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -74,6 +74,17 @@ if [ -n $MSYSTEM ]
   set -gx MSYS winsymlinks:nativestrict
 end
 
+########
+# fisher
+set -g fisher_path $__fish_config_dir/fisher
+
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..]
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..]
+
+for file in $fisher_path/conf.d/*.fish
+    source $file
+end
+
 test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 
 functions -e __add_path
