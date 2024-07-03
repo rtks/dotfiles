e4dddf4a252d4db12ecfaa06adeb713ae7c627ff
rtks <rtks@users.noreply.github.com>
2024-07-03 09:46:00 +0900

    fzfのgit logで全ブランチを表示、短縮Hashを使う

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,10 +1,10 @@
 function fish_user_key_bindings
   bind \cq '__fzf_z'
-  bind \eg '_fzf_search_git_log'
+  bind \eg '_fzf_search_git_log_custom'
 
   if ! test "$fish_key_bindings" = fish_default_key_bindings
     bind -M insert \cq '__fzf_z'
-    bind -M insert \eg '_fzf_search_git_log'
+    bind -M insert \eg '_fzf_search_git_log_custom'
   end
 
   bind \cd 'delete-or-deactivate-or-exit'
