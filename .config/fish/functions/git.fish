6026dae774b5da02c9c6546a628e727bd78a29b6
rtks <rtks@users.noreply.github.com>
2024-07-03 10:44:41 +0900

    git username未設定でpushする前に警告

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -3,6 +3,15 @@ function git
     if functions -q $subcommand
         set -e argv[1]
         eval $subcommand $opts $argv
+    else if command git rev-parse --git-dir >/dev/null 2>&1;
+            and test "$argv[1]" = "push";
+            and test -z "$(command git config --get --local user.name)";
+            and test "$(command git config --get user.name)" != "rtks"
+        echo "Git username is not yet set. Set up a username and email with"
+        echo "  git config --local user.name rtks"
+        echo "  git config --local user.email rtks@users.noreply.github.com"
+        echo "Or, without setting them, continue with"
+        echo "  command git $argv"
     else
         command git $opts $argv
     end
