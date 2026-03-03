cd61d10861d292816fec708a2159100fd27ec1bb
rtks <rtks@users.noreply.github.com>
2026-03-03 16:28:32 +0900

    fish 4.3より前のthemeを引き継ぎ

---
+++ .config/fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,38 @@
+# This file was created by fish when upgrading to version 4.3, to migrate
+# theme variables from universal to global scope.
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+# See also the release notes for fish 4.3.0 (run `help relnotes`).
+
+set --global fish_color_autosuggestion brcyan
+set --global fish_color_cancel -r
+set --global fish_color_command green
+set --global fish_color_comment 93a1a1
+set --global fish_color_cwd green
+set --global fish_color_cwd_root red
+set --global fish_color_end blue
+set --global fish_color_error brred
+set --global fish_color_escape cyan
+set --global fish_color_history_current cyan
+set --global fish_color_host normal
+set --global fish_color_host_remote yellow
+set --global fish_color_match cyan
+set --global fish_color_normal normal
+set --global fish_color_operator cyan
+set --global fish_color_param bryellow
+set --global fish_color_quote cyan
+set --global fish_color_redirection yellow
+set --global fish_color_search_match --background=grey
+set --global fish_color_selection --background=grey
+set --global fish_color_status red
+set --global fish_color_user normal
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_completion normal
+set --global fish_pager_color_description yellow
+set --global fish_pager_color_prefix cyan
+set --global fish_pager_color_progress cyan
+set --global fish_pager_color_selected_background -r
